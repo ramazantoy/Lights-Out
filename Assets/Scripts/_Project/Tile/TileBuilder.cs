acdde2785de77020004cf3b9c25d7f4c946222a4
Ramazan  TOY <ramcotoy06@gmail.com>
2023-05-02 22:57:34 +0300

    up

--- Assets/Scripts/_Project/Tile/TileBuilder.cs
+++ Assets/Scripts/_Project/Tile/TileBuilder.cs
@@ -1,222 +1,223 @@
-using System;
 using System.Collections.Generic;
 using _Project.Matrix;
-using _Project.Tile;
 using UnityEngine;
 using Random = System.Random;
 
-public class TileBuilder : MonoBehaviour
+namespace _Project.Tile
 {
-    public static TileBuilder Instance;
-
-    [SerializeField] private TileBuilderData _properties;
-
-    void Awake()
+    public class TileBuilder : MonoBehaviour
     {
-        if (Instance == null)
+        public static TileBuilder Instance;
+
+        [SerializeField] private TileBuilderData _properties;
+
+        void Awake()
         {
-            Instance = this;
-        }
-        else if (Instance != this)
-        {
-            Destroy(gameObject);
-        }
-    }
-    
-    public List<Tile> BuildRandomTiles()
-    {
-        SetCameraState(_properties.Row, _properties.Column);
-
-        MatrixInfo[,] matrixInfos = new MatrixInfo[_properties.Row, _properties.Column];
-
-        Vector3 startPos = new Vector3(0, 0, 0);
-
-        List<Tile> tiles = new List<Tile>();
-
-
-        for (int i = 0; i < _properties.Row; i++)
-        {
-            for (int j = 0; j < _properties.Column; j++)
+            if (Instance == null)
             {
-                int rate = UnityEngine.Random.Range(0, 101);
-                int value;
+                Instance = this;
+            }
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    
+        public List<Tile> BuildRandomTiles()
+        {
+            SetCameraState(_properties.Row, _properties.Column);
 
-                if (rate < _properties.LightOnRate)
+            MatrixInfo[,] matrixInfos = new MatrixInfo[_properties.Row, _properties.Column];
+
+            Vector3 startPos = new Vector3(0, 0, 0);
+
+            List<Tile> tiles = new List<Tile>();
+
+
+            for (int i = 0; i < _properties.Row; i++)
+            {
+                for (int j = 0; j < _properties.Column; j++)
                 {
-                    value = 1; //LightOn
-                }
-                else
-                {
-                    rate = UnityEngine.Random.Range(0, 101);
-                    if (rate < _properties.EmptyRate)
+                    int rate = UnityEngine.Random.Range(0, 101);
+                    int value;
+
+                    if (rate < _properties.LightOnRate)
                     {
-                        value = -1; //Empty
+                        value = 1; //LightOn
                     }
                     else
                     {
-                        value = 0; //LightOff
+                        rate = UnityEngine.Random.Range(0, 101);
+                        if (rate < _properties.EmptyRate)
+                        {
+                            value = -1; //Empty
+                        }
+                        else
+                        {
+                            value = 0; //LightOff
+                        }
+                    }
+
+                    matrixInfos[i, j] = new MatrixInfo(i, j, value);
+                }
+            }
+        
+
+            bool[,] unreachable = FindUnreachableTiles(matrixInfos);
+
+            for (int row = 0; row < _properties.Row; row++)
+            {
+                for (int col = 0; col < _properties.Column; col++)
+                {
+                    if (!unreachable[row, col])
+                    {
+                        matrixInfos[row, col].Value = -1;
                     }
                 }
-
-                matrixInfos[i, j] = new MatrixInfo(i, j, value);
             }
-        }
-        
 
-        bool[,] unreachable = FindUnreachableTiles(matrixInfos);
-
-        for (int row = 0; row < _properties.Row; row++)
-        {
-            for (int col = 0; col < _properties.Column; col++)
+            for (int i = 0; i < _properties.Row; i++)
             {
-                if (!unreachable[row, col])
+                for (int j = 0; j < _properties.Column; j++)
                 {
-                    matrixInfos[row, col].Value = -1;
+                    Vector3 spawnPos = startPos + new Vector3(j * (_properties.Spacing + 1),
+                        -i * (_properties.Spacing + 1), 0); // Her bir tile'ın pozisyonu +1'in sebebi tilerın genişliği ve uzunluğu 1 birim
+
+                    Tile tileObject = TilePool.Instance.TakeTile();
+                    tileObject.gameObject.name = "[" + i + "," + j + "] Tile";
+                    tileObject.transform.position = spawnPos;
+                    tileObject.transform.parent = transform;
+                    tileObject.GetComponent<Tile>().SetMatrixInfo(i, j, matrixInfos[i, j].Value);
+                    tileObject.gameObject.SetActive(true);
+                    tiles.Add(tileObject);
                 }
             }
+
+            TileManager.Instance.MatrixInfo = matrixInfos;
+            return tiles;
         }
 
-        for (int i = 0; i < _properties.Row; i++)
+
+        public List<Tile> BuildSavedTiles(MatrixInfo[,] matrixInfos)
         {
-            for (int j = 0; j < _properties.Column; j++)
+            Vector3 startPos = new Vector3(0, 0, 0);
+
+            List<Tile> tiles = new List<Tile>();
+
+            int row = matrixInfos.GetLength(0);
+            int column = matrixInfos.GetLength(1);
+
+            SetCameraState(row, column);
+
+            for (int i = 0; i < row; i++)
             {
-                Vector3 spawnPos = startPos + new Vector3(j * (_properties.Spacing + 1),
-                    -i * (_properties.Spacing + 1), 0); // Her bir tile'ın pozisyonu +1'in sebebi tilerın genişliği ve uzunluğu 1 birim
+                for (int j = 0; j < column; j++)
+                {
+                    Vector3 spawnPos = startPos + new Vector3(j * (_properties.Spacing + 1), -i * (_properties.Spacing + 1), 0); // Her bir tile'ın pozisyonu
 
-                Tile tileObject = TilePool.Instance.TakeTile();
-                tileObject.gameObject.name = "[" + i + "," + j + "] Tile";
-                tileObject.transform.position = spawnPos;
-                tileObject.transform.parent = transform;
-                tileObject.GetComponent<Tile>().SetMatrixInfo(i, j, matrixInfos[i, j].Value);
-                tileObject.gameObject.SetActive(true);
-                tiles.Add(tileObject);
+                    Tile tileObject = TilePool.Instance.TakeTile();
+                    tileObject.gameObject.name = "[" + i + "," + j + "] Tile";
+                    tileObject.transform.position = spawnPos;
+                    tileObject.transform.parent = transform;
+                    tileObject.GetComponent<Tile>().SetMatrixInfo(i, j, matrixInfos[i,j].Value);
+                    tileObject.gameObject.SetActive(true);
+                    tiles.Add(tileObject);
+                }
             }
-        }
 
-        TileManager.Instance.MatrixInfo = matrixInfos;
-        return tiles;
-    }
-
-
-    public List<Tile> BuildSavedTiles(MatrixInfo[,] matrixInfos)
-    {
-        Vector3 startPos = new Vector3(0, 0, 0);
-
-        List<Tile> tiles = new List<Tile>();
-
-        int row = matrixInfos.GetLength(0);
-        int column = matrixInfos.GetLength(1);
-
-        SetCameraState(row, column);
-
-        for (int i = 0; i < row; i++)
-        {
-            for (int j = 0; j < column; j++)
-            {
-                Vector3 spawnPos = startPos + new Vector3(j * (_properties.Spacing + 1), -i * (_properties.Spacing + 1), 0); // Her bir tile'ın pozisyonu
-
-                Tile tileObject = TilePool.Instance.TakeTile();
-                tileObject.gameObject.name = "[" + i + "," + j + "] Tile";
-                tileObject.transform.position = spawnPos;
-                tileObject.transform.parent = transform;
-                tileObject.GetComponent<Tile>().SetMatrixInfo(i, j, matrixInfos[i,j].Value);
-                tileObject.gameObject.SetActive(true);
-                tiles.Add(tileObject);
-            }
-        }
-
-        TileManager.Instance.MatrixInfo = matrixInfos;
+            TileManager.Instance.MatrixInfo = matrixInfos;
     
 
-        return tiles;
-    }
-
-    private void SetCameraState(int row, int column)
-    {
-        float tileWidth = 1f; //Tile genişliği
-        float tileHeight = 1f; //Tile Yüksekliği
-
-        float totalWidth = _properties.Column * (tileWidth + _properties.Spacing) - _properties.Spacing +
-                           2 * _properties
-                               .Padding; //kenar boşluğu ve tilelar arasındaki boşlukları hesaba katarak toplam genişliğin hesaplanması
-        float totalHeight = _properties.Row * (tileHeight + _properties.Spacing) - _properties.Spacing +
-                            2 * _properties
-                                .Padding; //kenar boşluğu ve tilelar arasındaki boşlukları hesaba katarak toplam yüksekliğim hesaplanması
-
-        //Elde edilen genişlik ve yüksekliğe göre kameranın ayarlanması
-        float cameraSize = Mathf.Max(totalHeight / 2f, totalWidth / 2f / Camera.main.aspect);
-        Camera.main.orthographicSize = cameraSize;
-
-
-        Vector3 cameraPos = new Vector3(totalWidth / 2f - tileWidth / 2f - _properties.Padding,
-            -totalHeight / 2f + tileHeight / 2f + _properties.Padding, -cameraSize);
-        Camera.main.transform.position = cameraPos;
-    }
-
-    //Bfs Algoritması random oluşturulan matrix üzerinde  selectable olan her bir noktaya kuyruğa ekleniyor
-    private bool[,] FindUnreachableTiles(MatrixInfo[,] matrixInfo)
-    {
-        int rowCount = matrixInfo.GetLength(0);
-        int colCount = matrixInfo.GetLength(1);
-        MatrixHandler matrixHandler = new MatrixHandler(matrixInfo);
-        bool[,] visited = new bool[rowCount, colCount];
-
-        Queue<MatrixInfo> queue = new Queue<MatrixInfo>();
-
-
-        MatrixInfo start = null;
-        while (start == null)
-        {
-            int randomRow = new Random().Next(rowCount);
-            int randomCol = new Random().Next(colCount);
-            MatrixInfo randomElement = matrixInfo[randomRow, randomCol];
-            if ((randomElement.Value == 1 || randomElement.Value == 0) &&
-                matrixHandler.GetMyNeighbour(randomElement).Count > 0)
-            {
-                start = randomElement;
-            }
+            return tiles;
         }
 
-        visited[start.Row, start.Column] = true;
-        queue.Enqueue(start);
-
-        // Eleman eleman BFS işlemi
-        while (queue.Count > 0)
+        private void SetCameraState(int row, int column)
         {
-            MatrixInfo current = queue.Dequeue();
-            List<MatrixInfo> currentNeighbours = matrixHandler.GetMyNeighbour(current);
+            float tileWidth = 1f; //Tile genişliği
+            float tileHeight = 1f; //Tile Yüksekliği
 
-            // Komşular kuyruğa ekleniyor
-            foreach (MatrixInfo neighbour in currentNeighbours)
+            float totalWidth = _properties.Column * (tileWidth + _properties.Spacing) - _properties.Spacing +
+                               2 * _properties
+                                   .Padding; //kenar boşluğu ve tilelar arasındaki boşlukları hesaba katarak toplam genişliğin hesaplanması
+            float totalHeight = _properties.Row * (tileHeight + _properties.Spacing) - _properties.Spacing +
+                                2 * _properties
+                                    .Padding; //kenar boşluğu ve tilelar arasındaki boşlukları hesaba katarak toplam yüksekliğim hesaplanması
+
+            //Elde edilen genişlik ve yüksekliğe göre kameranın ayarlanması
+            float cameraSize = Mathf.Max(totalHeight / 2f, totalWidth / 2f / Camera.main.aspect);
+            Camera.main.orthographicSize = cameraSize;
+
+
+            Vector3 cameraPos = new Vector3(totalWidth / 2f - tileWidth / 2f - _properties.Padding,
+                -totalHeight / 2f + tileHeight / 2f + _properties.Padding, -cameraSize);
+            Camera.main.transform.position = cameraPos;
+        }
+
+        //Bfs Algoritması random oluşturulan matrix üzerinde  selectable olan her bir noktaya kuyruğa ekleniyor
+        private bool[,] FindUnreachableTiles(MatrixInfo[,] matrixInfo)
+        {
+            int rowCount = matrixInfo.GetLength(0);
+            int colCount = matrixInfo.GetLength(1);
+            MatrixHandler matrixHandler = new MatrixHandler(matrixInfo);
+            bool[,] visited = new bool[rowCount, colCount];
+
+            Queue<MatrixInfo> queue = new Queue<MatrixInfo>();
+
+
+            MatrixInfo start = null;
+            while (start == null)
             {
-                int nRow = neighbour.Row;
-                int nCol = neighbour.Column;
-
-                if (!visited[nRow, nCol] && neighbour.Value != -1)
+                int randomRow = new Random().Next(rowCount);
+                int randomCol = new Random().Next(colCount);
+                MatrixInfo randomElement = matrixInfo[randomRow, randomCol];
+                if ((randomElement.Value == 1 || randomElement.Value == 0) &&
+                    matrixHandler.GetMyNeighbour(randomElement).Count > 0)
                 {
-                    visited[nRow, nCol] = true;
-                    queue.Enqueue(neighbour);
+                    start = randomElement;
                 }
             }
-        }
-        //Geçilebilecek siyah nokta döşe
 
-        // Ziyaret edilmeyen elemanlar işaretleniyor
-        for (int row = 0; row < rowCount; row++)
-        {
-            for (int col = 0; col < colCount; col++)
+            visited[start.Row, start.Column] = true;
+            queue.Enqueue(start);
+
+            // Eleman eleman BFS işlemi
+            while (queue.Count > 0)
             {
-                if ((matrixInfo[row, col].Value == 1 || matrixInfo[row, col].Value == 0) && !visited[row, col])
+                MatrixInfo current = queue.Dequeue();
+                List<MatrixInfo> currentNeighbours = matrixHandler.GetMyNeighbour(current);
+
+                // Komşular kuyruğa ekleniyor
+                foreach (MatrixInfo neighbour in currentNeighbours)
                 {
-                    visited[row, col] = false;
+                    int nRow = neighbour.Row;
+                    int nCol = neighbour.Column;
+
+                    if (!visited[nRow, nCol] && neighbour.Value != -1)
+                    {
+                        visited[nRow, nCol] = true;
+                        queue.Enqueue(neighbour);
+                    }
                 }
             }
-        }
+            //Geçilebilecek siyah nokta döşe
+
+            // Ziyaret edilmeyen elemanlar işaretleniyor
+            for (int row = 0; row < rowCount; row++)
+            {
+                for (int col = 0; col < colCount; col++)
+                {
+                    if ((matrixInfo[row, col].Value == 1 || matrixInfo[row, col].Value == 0) && !visited[row, col])
+                    {
+                        visited[row, col] = false;
+                    }
+                }
+            }
         
-        return visited;
-    }
+            return visited;
+        }
 
 
  
 
+    }
 }
